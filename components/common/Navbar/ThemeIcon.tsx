6582c6bd13ddf65a8fbd46ee23ff0338d9676c35
ansango <anibalsantosgo@gmail.com>
2022-04-15 20:45:27 +0200

    toggle

--- components/common/Navbar/ThemeIcon.tsx
+++ components/common/Navbar/ThemeIcon.tsx
@@ -18,31 +18,35 @@ const ThemeIcon = () => {
   }, [theme]);
   useEffect(() => setMounted(true), []);
 
-  if (!mounted && user) return null;
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-      className="tooltip tooltip-left"
-      data-tip={theme === "light" ? "Tema oscuro" : "Tema claro"}
-    >
-      <button
-        className="btn btn-ghost btn-circle"
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      >
-        {theme === "light" ? (
-          <>
-            <Icon kind="outline" icon="MoonIcon" className="w-5 h-4" />
-          </>
-        ) : (
-          <>
-            <Icon kind="outline" icon="SunIcon" className="w-4 h-4" />
-          </>
+  if (!mounted) return null;
+    return (
+      <>
+        {!user && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="tooltip tooltip-left"
+            data-tip={theme === "light" ? "Tema oscuro" : "Tema claro"}
+          >
+            <button
+              className="btn btn-ghost btn-circle"
+              onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+            >
+              {theme === "light" ? (
+                <>
+                  <Icon kind="outline" icon="MoonIcon" className="w-5 h-4" />
+                </>
+              ) : (
+                <>
+                  <Icon kind="outline" icon="SunIcon" className="w-4 h-4" />
+                </>
+              )}
+            </button>
+          </motion.div>
         )}
-      </button>
-    </motion.div>
-  );
+      </>
+    );
 };
 
 export default ThemeIcon;
