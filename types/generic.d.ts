d9be3c21b5227931d0385f036b3187e0bb81bc8a
ansango <anibalsantosgo@gmail.com>
2022-04-15 18:35:45 +0200

    dependencies

---
+++ types/generic.d.ts
@@ -0,0 +1,57 @@
+// Ids
+
+type UserId = ObjectId | string;
+type AccountId = ObjectId | string;
+type ChatId = ObjectId | string;
+type RecipeId = ObjectId | string;
+type BlenderId = ObjectId | string;
+type CategoryId = ObjectId | string;
+type CommentId = ObjectId | string;
+type CollectionId = ObjectId | string;
+type MessageId = ObjectId | string;
+type TokenId = ObjectId | string;
+
+// Generics
+
+type Name = string;
+type Content = string;
+
+// User & Account
+
+type Username = string;
+type FirstName = string;
+type LastName = string;
+type Email = string;
+type Url = string;
+type Password = string;
+type Phone = string;
+type Birthday = string;
+type City = string;
+type Zip = string;
+type Country = string;
+
+type CompanyName = Name;
+// Recipe
+
+type Servings = number;
+type Duration = number;
+type Position = number;
+
+// Category
+
+type CategoryName = Name;
+
+// Ingredient
+
+type Unit = number;
+type Measure = "gr" | "portion";
+
+// Blender
+
+type BlenderName =
+  | "Thermomix"
+  | "Mambo"
+  | "KCook"
+  | "Maxichef"
+  | "MyCook"
+  | "Monsieur Cusine";
