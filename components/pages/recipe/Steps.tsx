10c8b7aec17f928406586c22818eff36ae783360
ansango <anibalsantosgo@gmail.com>
2022-04-23 04:37:26 +0200

    user blocks

--- components/pages/recipe/Steps.tsx
+++ components/pages/recipe/Steps.tsx
@@ -1,9 +1,4 @@
 import { RecipeDTO } from "@/models/recipe/recipe";
-import { selectUser } from "@/store/features/user";
-import { useAppSelector } from "@/store/hooks";
-import ButtonLink from "components/common/Button/ButtonLink";
-import { Icon } from "components/common/Icons";
-
 import { FC } from "react";
 import StepC from "./Step";
 
@@ -12,23 +7,12 @@ type Props = {
 };
 
 const Steps: FC<Props> = ({ steps }) => {
-  const user = useAppSelector(selectUser);
   return (
-    <>
-      {user && (
-        <ul className="space-y-5">
-          {steps.map((step, index) => (
-            <StepC key={index} step={step} />
-          ))}
-        </ul>
-      )}
-      {!user && (
-        <div className="space-y-5 px-3 flex sm:justify-end items-end space-x-2">
-          <Icon icon="LoginIcon" kind="outline" className="text-primary w-5 h-5 mb-0.5" />
-          <ButtonLink href="/sigin" label="Inicia session para ver los pasos" />
-        </div>
-      )}
-    </>
+    <ul className="space-y-5">
+      {steps.map((step, index) => (
+        <StepC key={index} step={step} />
+      ))}
+    </ul>
   );
 };
 
--- components/pages/recipe/StepsBlock.tsx
+++ components/pages/recipe/StepsBlock.tsx
@@ -1,4 +1,7 @@
 import { RecipeDTO } from "@/models/recipe/recipe";
+import { selectUser } from "@/store/features/user";
+import { useAppSelector } from "@/store/hooks";
+import ButtonLink from "components/common/Button/ButtonLink";
 import { Icon } from "components/common/Icons";
 import React, { FC } from "react";
 import Steps from "./Steps";
@@ -9,8 +12,25 @@ type Props = {
 };
 
 const StepsBlock: FC<Props> = ({ steps }) => {
+  const user = useAppSelector(selectUser);
+  const cn = !user ? "relative h-60 overflow-hidden" : "";
   return (
-    <article>
+    <article className={cn}>
+      {!user && (
+        <div className="absolute w-full h-full bg-gradient-to-b from-transparent via-base-100 to-base-100">
+          <div className="space-y-5 px-3 flex sm:justify-end items-end space-x-2 absolute bottom-0 right-0">
+            <Icon
+              icon="LoginIcon"
+              kind="outline"
+              className="text-primary w-5 h-5 mb-0.5"
+            />
+            <ButtonLink
+              href="/signin"
+              label="Inicia session para ver los pasos"
+            />
+          </div>
+        </div>
+      )}
       <div className="p-5 rounded-lg space-y-3 bg-base-200">
         <div className="flex items-center space-x-2 px-3">
           <Subtitle text="Preparación" />
