76ecf4d3e1503e474798f1f7171ffc7f51afa07b
ansango <anibalsantosgo@gmail.com>
2022-04-23 03:54:02 +0200

    block

--- components/pages/recipe/Printer.tsx
+++ components/pages/recipe/Printer.tsx
@@ -1,13 +1,20 @@
+import { selectUser } from "@/store/features/user";
+import { useAppSelector } from "@/store/hooks";
 import { Icon } from "components/common/Icons";
 import { FC } from "react";
 
 type Props = {};
 
 const Printer: FC<Props> = () => {
+  const user = useAppSelector(selectUser);
   return (
-    <button className="btn btn-ghost btn-sm btn-circle">
-      <Icon icon="PrinterIcon" kind="outline" className="w-5 h-5" />
-    </button>
+    <>
+      {user && (
+        <button className="btn btn-ghost btn-sm btn-circle">
+          <Icon icon="PrinterIcon" kind="outline" className="w-5 h-5" />
+        </button>
+      )}
+    </>
   );
 };
 
