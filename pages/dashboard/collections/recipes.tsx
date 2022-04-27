a3d63a5e45ac5d75026f92a688ad80d7404f30d8
ansango <anibalsantosgo@gmail.com>
2022-04-28 00:48:30 +0200

    table

--- pages/dashboard/collections/recipes.tsx
+++ pages/dashboard/collections/recipes.tsx
@@ -1,3 +1,4 @@
+import { Recipe } from "@/models/recipe/recipe";
 import { selectBookmarkRecipes } from "@/store/features/account/bookmark";
 import { useAppSelector } from "@/store/hooks";
 import Breadcrumb from "components/common/Breadcrumb/Breadcrumb";
@@ -49,76 +50,89 @@ const Recipes: NextPage = () => {
             <tbody>
               {recipes && recipes.length > 0 ? (
                 <>
-                  {recipes.map((recipe, i) => (
-                    <tr key={i}>
-                      <th>
-                        <label>
-                          <input type="checkbox" className="checkbox" />
-                        </label>
-                      </th>
-                      <td>
-                        <div className="flex items-center space-x-3">
-                          <div className="avatar">
-                            <div className="mask mask-squircle w-12 h-12">
-                              <Image
-                                src={recipe.img}
-                                loading="lazy"
-                                alt="hero"
-                                layout="fill"
-                                className="object-center object-cover pointer-events-none"
-                              />
+                  {recipes.map(
+                    (
+                      {
+                        img,
+                        name,
+                        description,
+                        duration,
+                        servings,
+                        blenders,
+                        categories,
+                      }: Recipe,
+                      i
+                    ) => (
+                      <tr key={i}>
+                        <th>
+                          <label>
+                            <input type="checkbox" className="checkbox" />
+                          </label>
+                        </th>
+                        <td>
+                          <div className="flex items-center space-x-3">
+                            <div className="avatar">
+                              <div className="mask mask-squircle w-12 h-12">
+                                <Image
+                                  src={img}
+                                  loading="lazy"
+                                  alt="hero"
+                                  layout="fill"
+                                  className="object-center object-cover pointer-events-none"
+                                />
+                              </div>
+                            </div>
+                            <div>
+                              <div className="font-bold">{name}</div>
+                              <div className="text-sm opacity-50 capitalize">
+                                {blenders[0]}
+                              </div>
                             </div>
                           </div>
-                          <div>
-                            <div className="font-bold">{recipe.name}</div>
-                            <div className="text-sm opacity-50 capitalize">
-                              {recipe.blenders[0]}
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="max-w-xs">
-                        <p className="truncate">{recipe.description}</p>
-                        {recipe.categories.slice(0, 3).map((category, i) => (
-                          <span
-                            key={i}
-                            className="badge badge-ghost badge-sm capitalize mr-1"
-                          >
-                            {category}
+                        </td>
+                        <td className="max-w-xs">
+                          <p className="truncate">{description}</p>
+                          {categories.slice(0, 3).map((category, i) => (
+                            <span
+                              key={i}
+                              className="badge badge-ghost badge-sm capitalize mr-1"
+                            >
+                              {category}
+                            </span>
+                          ))}
+                        </td>
+                        <td>
+                          <span className="badge badge-ghost badge-sm capitalize">
+                            <Icon
+                              icon="UserGroupIcon"
+                              kind="outline"
+                              className="w-4 h-4 mr-1"
+                            />
+                            {servings}
                           </span>
-                        ))}
-                      </td>
-                      <td>
-                        <span className="badge badge-ghost badge-sm capitalize">
-                          <Icon
-                            icon="UserGroupIcon"
-                            kind="outline"
-                            className="w-4 h-4 mr-1"
-                          />
-                          {recipe.servings}
-                        </span>
-                      </td>
-                      <td>
-                        <span className="badge badge-ghost badge-sm capitalize mr-1">
-                          <Icon
-                            icon="ClockIcon"
-                            kind="outline"
-                            className="w-4 h-4 mr-1"
-                          />
-                          {recipe.duration}
-                        </span>
-                      </td>
-                      <th>
-                        <button className="btn btn-ghost btn-sm btn-circle">
-                          <Icon
-                            icon="EyeIcon"
-                            kind="outline"
-                            className="w-5 h-5 text-primary"
-                          />
-                        </button>
-                      </th>
-                    </tr>
-                  ))}
+                        </td>
+                        <td>
+                          <span className="badge badge-ghost badge-sm capitalize mr-1">
+                            <Icon
+                              icon="ClockIcon"
+                              kind="outline"
+                              className="w-4 h-4 mr-1"
+                            />
+                            {duration}
+                          </span>
+                        </td>
+                        <th>
+                          <button className="btn btn-ghost btn-sm btn-circle">
+                            <Icon
+                              icon="EyeIcon"
+                              kind="outline"
+                              className="w-5 h-5 text-primary"
+                            />
+                          </button>
+                        </th>
+                      </tr>
+                    )
+                  )}
                 </>
               ) : null}
             </tbody>
