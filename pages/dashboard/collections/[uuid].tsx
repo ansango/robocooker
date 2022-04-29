1c0b1f7230ba627c56c3456e0ce01a77f7982135
ansango <anibalsantosgo@gmail.com>
2022-04-29 21:01:20 +0200

    tabs

--- pages/dashboard/collections/[uuid].tsx
+++ pages/dashboard/collections/[uuid].tsx
@@ -1,3 +1,4 @@
+import { Collection } from "@/models/user/bookmark";
 import { selectBookmark } from "@/store/features/account/bookmark";
 import { useAppSelector } from "@/store/hooks";
 import Breadcrumb from "components/common/Breadcrumb/Breadcrumb";
@@ -8,7 +9,62 @@ import ContainerDashboard from "components/dashboard/ContainerDashboard";
 import DashboardLayout from "components/layout/DashboardLayout";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import React from "react";
+import { FC } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+
+const Tabs: FC<{ uuid: Collection["uuid"] }> = ({ uuid }) => {
+  return (
+    <div className="tabs flex justify-end">
+      <NavLink
+        to={`/dashboard/collections/${uuid}`}
+        className={({ isActive }) =>
+          isActive ? "tab tab-lifted tab-active" : "tab tab-lifted"
+        }
+        end
+      >
+        Recetas
+      </NavLink>
+
+      <NavLink
+        to={`/dashboard/collections/${uuid}/ingredients`}
+        className={({ isActive }) =>
+          isActive ? "tab tab-lifted tab-active" : "tab tab-lifted"
+        }
+      >
+        Ingredientes
+      </NavLink>
+    </div>
+  );
+};
+
+const CollectionView: FC<{ uuid: Collection["uuid"] }> = ({ uuid }) => {
+  return (
+    <Router>
+      <div>
+        <Tabs uuid={uuid} />
+        <div className="rounded-b-box shadow-sm">
+          <div className="preview bg-base-100 rounded-b-box rounded-tl-box p-5 border-r">
+            <Routes>
+              <Route
+                path={`/dashboard/collections/:uuid/ingredients`}
+                element={<div>ingredients</div>}
+              />
+              <Route
+                path={`/dashboard/collections/:uuid`}
+                element={<div>Hello</div>}
+              />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+};
 
 const Collection: NextPage = () => {
   const { query } = useRouter();
@@ -28,10 +84,11 @@ const Collection: NextPage = () => {
             icon={{ icon: "BookmarkIcon", kind: "outline" }}
           />
           <BreadcrumbNoLink
-            label={collection?.name}
+            label={collection.name}
             icon={{ icon: "BookmarkAltIcon", kind: "outline" }}
           />
         </Breadcrumb>
+        <CollectionView uuid={collection.uuid} />
       </ContainerDashboard>
     </DashboardLayout>
   );
