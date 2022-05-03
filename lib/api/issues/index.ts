5e7c7c69b992ea60352d90e1361b825031c0f531
ansango <anibalsantosgo@gmail.com>
2022-05-03 05:19:56 +0200

    refactor notion

--- lib/api/issues/index.ts
+++ lib/api/issues/index.ts
@@ -2,7 +2,7 @@ import { Client } from "@notionhq/client";
 
 export const getClientNotion = () => {
   const notion = new Client({ auth: process.env.NOTION_KEY });
-  const databaseId = process.env.NOTION_DATABASE_ID;
+  const databaseId = process.env.NOTION_DATABASE_ID ?? "";
   return {
     notion,
     database_id: databaseId,
