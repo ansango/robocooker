d9a674d4d42cc7f0ec8bc03edfa6aecaadb536bf
ansango <anibalsantosgo@gmail.com>
2022-06-02 23:08:28 +0200

    robousers

--- pages/robousers.tsx
+++ pages/robousers.tsx
@@ -11,6 +11,21 @@ import { NextPage } from "next";
 import Link from "next/link";
 import { useEffect } from "react";
 
+const placeEmojis = [
+  {
+    name: "first",
+    emoji: "🥇",
+  },
+  {
+    name: "second",
+    emoji: "🥈",
+  },
+  {
+    name: "third",
+    emoji: "🥉",
+  },
+];
+
 const Robousers: NextPage = () => {
   const dispatch = useAppDispatch();
   const robousers = useAppSelector(selectRobousers);
@@ -23,33 +38,85 @@ const Robousers: NextPage = () => {
         title="Robousers"
         description="Robousers es una comunidad de usuarios que aman la cocina y la comida. ¡Sé parte de esta comunidad!"
       />
-      <Container>
-        <ContainerHeader>
-          <Subtitle title="Los que mas se manchan" />
-          <SubParagraph content="¿Quieres ser parte de esta comunidad? ¡Sé parte de esta comunidad!" />
-        </ContainerHeader>
-        <ContainerContent>
-          {robousers &&
-            robousers.slice(0, 3).map((robouser, index) => (
-              <div key={index}>
-                <Avatar
-                  size="lg"
-                  username={robouser.username}
-                  imgUrl={robouser.avatar}
-                />
-                <h4 className="font-semibold text-xl">@{robouser.username}</h4>
-                <p className="text-gray-600 dark:text-gray-400">
-                  {robouser.about}
-                </p>
-                <Link passHref href={`/profile/${robouser.username}`}>
-                  <button className="btn btn-outline btn-primary normal-case btn-sm">
-                    Perfil
-                  </button>
-                </Link>
+      <div className="">
+        <Container>
+          <ContainerHeader>
+            <Subtitle title="Los que mas se manchan" />
+            <SubParagraph content="Hay mucha gente creando nuevas recetas, pero los que están a fuego merecen un lugar en nuestros fogones." />
+          </ContainerHeader>
+          <ContainerContent>
+            <div className="bg-gradient-to-r from-[#a7ecd0] to-[#b565ff] dark:from-[#341e49] dark:to-[#374d44] rounded-2xl p-10 flex justify-center">
+              <div className="space-y-10">
+                {robousers &&
+                  robousers.slice(0, 3).map((robouser, index) => (
+                    <div key={index} className="card shadow-xl glass">
+                      <div className="flex flex-col items-center space-y-5 card-body">
+                        <div className="flex items-center">
+                          {placeEmojis[index] && (
+                            <span className="text-5xl">
+                              {placeEmojis[index].emoji}
+                            </span>
+                          )}
+                          <Avatar
+                            size="lg"
+                            username={robouser.username}
+                            imgUrl={robouser.avatar}
+                          />
+                          {placeEmojis[index] && (
+                            <span className="text-5xl">
+                              {placeEmojis[index].emoji}
+                            </span>
+                          )}
+                        </div>
+                        <h4 className="font-semibold text-xl">
+                          @{robouser.username}
+                        </h4>
+                        <Link passHref href={`/profile/${robouser.username}`}>
+                          <button className="btn btn-primary normal-case btn-sm">
+                            Perfil
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
+                  ))}
               </div>
-            ))}
-        </ContainerContent>
-      </Container>
+            </div>
+          </ContainerContent>
+        </Container>
+      </div>
+      <div className="bg-gray-50 dark:bg-gray-800 py-5">
+        <Container>
+          <ContainerHeader>
+            <Subtitle title="Subiendo posiciones" />
+            <SubParagraph content="Y escalando posiciones en la comunidad de Robousers todos estos aspirantes a top chefs" />
+          </ContainerHeader>
+          <ContainerContent>
+            <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+              {robousers &&
+                robousers.slice(3, 11).map((robouser, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col items-center space-y-5 mb-5"
+                  >
+                    <Avatar
+                      size="md"
+                      username={robouser.username}
+                      imgUrl={robouser.avatar}
+                    />
+                    <h4 className="font-semibold text-xl">
+                      @{robouser.username}
+                    </h4>
+                    <Link passHref href={`/profile/${robouser.username}`}>
+                      <button className="btn btn-primary normal-case btn-sm">
+                        Perfil
+                      </button>
+                    </Link>
+                  </div>
+                ))}
+            </div>
+          </ContainerContent>
+        </Container>
+      </div>
     </MainLayout>
   );
 };
